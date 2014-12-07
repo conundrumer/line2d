26835d3360e052356d8a3b8add5b4b320880ede8
David Lu <davidelu93@gmail.com>
2014-12-07 18:14:11 -0500

    Got UMD working

--- src/line2d.ts
+++ src/line2d.ts
@@ -1,6 +1,22 @@
 module Line2D {
   export var NAME = "Line2D";
+
+  export function hi() {
+    console.log("hello")
+  }
 }
 
 declare var module;
-module.exports = Line2D;
+declare var define;
+interface Window { Line2D: any; }
+
+// https://github.com/umdjs/umd
+if (typeof module === "object" && module.exports) {
+  // CommonJS (Node)
+  module.exports = Line2D;
+} else if (typeof define === "function" && define.amd) {
+  // AMD
+  define(function () { return Line2D; });
+} else {
+  window.Line2D = Line2D;
+}
