af2e4884508427e90a0b164374aa8727227eaa16
David Lu <davidelu93@gmail.com>
2014-12-08 04:09:08 -0500

    Alter structure of input points/lines

--- index.js
+++ index.js
@@ -4,25 +4,22 @@ if (require) {
 } else {
     Line2D = window.Line2D
 }
+var P = Line2D.toPoints;
+var L = Line2D.toLines;
 
 var emptyScene = Line2D.newScene();
 
-// var p1 = new Line2D.Point('0', [0, 0]);
-// var p2 = new Line2D.Point('1', [1, 1]);
-
-// var l = new Line2D.Line('0', p1, p2);
-
 var s1 = emptyScene
     .addPoint('0_0', [0, 0])
-    .addPoints([
+    .addPoints(P([
         ['0_1', [0, 1]],
         ['0_2', [1, 0]]
-    ])
+    ]))
     .addLine('0_0', ['0_0', '0_1'])
-    .addLines([
+    .addLines(L([
         ['0_1', ['0_1', '0_2']],
         ['0_2', ['0_2', '0_0']]
-    ])
+    ]))
 
 console.log(JSON.stringify(s1));
 console.log(s1.toJS());
