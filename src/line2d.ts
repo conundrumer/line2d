4f5556fd55639bff4538fba6bb40fc5053e3a642
David Lu <davidelu93@gmail.com>
2014-12-13 02:26:54 -0500

    Significantly change API

--- src/line2d.ts
+++ src/line2d.ts
@@ -6,60 +6,97 @@ module Line2D {
 
     export type LineID = string;
     export type PointID = string;
-    export type PointTuple = [number, number];
-
-    export interface SceneObj {
-        points: PointsObj;
-        lines: LinesObj;
+    export type VecTuple = [number, number];
+    export type LengthRange = [number, number];
+    export interface LengthFn {
+        (prevLength: number, time: number): number
     }
-    export interface PointsObj {
-        [pid: string] : PointObjVec
-    }
-    export interface LinesObj {
-        [lid: string] : LineEndPoints
-    }
-    export interface PointObjVec {
+    export interface VecObj {
         x: number;
         y: number;
     }
-    export interface LineEndPoints {
-        p: PointID;
-        q: PointID;
+
+    export interface PointObj {
+        id: PointID;
+        pos: VecObj;
+        prevPos?: VecObj;
+        solid?: boolean;
+        props?: {
+            mass?: number;
+            airFriction?: number;
+            friction?: number;
+            restitution?: number;
+        };
+    }
+    export interface LineObj {
+        id: LineID;
+        pq: {
+            p: PointID;
+            q: PointID;
+        };
+        length?: number | LengthRange | LengthFn;
+        solid?: boolean;
+        singleSided?: boolean;
+        props?: {
+            airFriction?: number;
+            friction?: number;
+            restitution?: number;
+            acceleration?: number;
+            breakThreshold?: number;
+        }
     }
 
-    export function toPoints(pointProps: Array<[PointID, PointTuple]>) : PointsObj {
-        var points: PointsObj = {};
-        pointProps.forEach( p => {
-            points[p[0]] = { x: p[1][0], y: p[1][1] };
-        });
-        return points;
+    export function toPoints(pointProps: Array<[PointID, VecTuple]>) : PointsObj {
+        return pointProps.forEach( p => {
+            return {
+                id: p[0],
+                pos: {
+                    x: p[1][0],
+                    y: p[1][1]
+                }
+            }
+        })
     }
 
     export function toLines(lineProps: Array<[LineID, [PointID, PointID]]>) : LinesObj {
-        var lines: LinesObj = {};
-        lineProps.forEach( l => {
-            lines[l[0]] = { p: l[1][0], q: l[1][1] };
-        });
-        return lines;
+        return pointProps.forEach( p => {
+            return {
+                id: l[0],
+                pq: {
+                    p: l[1][0],
+                    q: l[1][1]
+                }
+            }
+        })
     }
 
     export interface Scene {
         toJS(): SceneObj;
-        addPoint(id: PointID, pos: PointTuple): Scene;
-        addPoints(points: PointsObj): Scene;
-        addLine(lid: LineID, pids: [PointID, PointID]): Scene;
-        addLines(lines: LinesObj): Scene;
+
+        getPoint(pid: PointID): VecObj;
+        getPoints(pids: Array<PointID>): PointsObj;
+        getLine(lid: LineID): LinePointIDs;
+        getLines(lids: Array<LineID>): LinesObj;
+
+        getLinesFromPoint(pid: PointID): Array<LineID>;
+        getLinesFromPoints(pids: Array<PointID>): { [pid: string]: Array<LineID>};
+        getLinesSetFromPoints(pids: Array<PointID>): Array<LineID>;
+
+        addPoint(point: PointObj): Scene;
+        addPoints(points: Array<PointObj>): Scene;
+        addLine(line: LineObj): Scene;
+        addLines(lines: Array<LineObj>): Scene;
+
         removePoint(pid: PointID): Scene;
         removePoints(pids: Array<PointID>): Scene;
         removeLine(lid: LineID): Scene;
         removeLines(lids: Array<LineID>): Scene;
+
+        eraseLine(lid: LineID): Scene;
+        eraseLines(lids: Array<LineID>): Scene;
     }
     export function newScene() : Scene {
         return Scene.create();
-    }
-
-    export function makeSceneFromJS(scene: SceneObj) : Scene {
-        return Scene.create().addPoints(scene.points).addLines(scene.lines);
     }
 }
 
