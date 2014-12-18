827159f54fe2aa9796385450d5a3e11f20226046
David Lu <davidelu93@gmail.com>
2014-12-18 02:39:11 -0500

    Change example to new API

--- index.js
+++ index.js
@@ -10,32 +10,32 @@ var L = Line2D.toLines;
 var emptyScene = Line2D.newScene();
 
 var s1 = emptyScene
-    .addPoint(P([['a', [0, 0]]])[0])
-    .addPoints(P([
+    .points.add(P([['a', [0, 0]]])[0])
+    .points.add(P([
         ['b', [0, 1]],
         ['c', [1, 0]]
     ]))
-    .addLine(L([['ab', ['a', 'b']]])[0])
-    .addLines(L([
+    .lines.add(L([['ab', ['a', 'b']]])[0])
+    .lines.add(L([
         ['bc', ['b', 'c']],
         ['ca', ['c', 'a']]
     ]))
 
 console.log('stringify', JSON.stringify(s1));
 console.log('toJS', s1.toJS());
-console.log('removeLine',JSON.stringify(s1.removeLine('ab')));
-console.log('removeLines',JSON.stringify(s1.removeLines(['ab','bc'])));
-console.log('removePoint',JSON.stringify(s1.removePoint('a')));
-console.log('removePoints',JSON.stringify(s1.removePoints(['a','b'])));
+console.log('removeLine',JSON.stringify(s1.lines.remove('ab')));
+console.log('removeLines',JSON.stringify(s1.lines.remove(['ab','bc'])));
+console.log('removePoint',JSON.stringify(s1.points.remove('a')));
+console.log('removePoints',JSON.stringify(s1.points.remove(['a','b'])));
 
-console.log('getPoint', s1.getPoint('a'))
-console.log('getPoints', s1.getPoints(['b','c']))
-console.log('getLine', s1.getLine('ab'))
-console.log('getLines', s1.getLines(['bc','ca']))
-console.log('getLinesFromPoint', s1.getLinesFromPoint('a'))
-console.log('getLinesFromPoints', s1.getLinesFromPoints(['b','c']))
+console.log('getPoint', s1.points.get('a'))
+console.log('getPoints', s1.points.get(['b','c']))
+console.log('getLine', s1.lines.get('ab'))
+console.log('getLines', s1.lines.get(['bc','ca']))
+console.log('getLinesFromPoint', s1.lines.getFromPoints('a'))
+console.log('getLinesFromPoints', s1.lines.getFromPoints(['b','c']))
 
-console.log('eraseLine', s1.eraseLine('ab').toJS())
-console.log('eraseLine', s1.eraseLine('ab').eraseLine('bc').toJS())
-console.log('eraseLines', s1.eraseLines(['bc','ca']).toJS())
-console.log('eraseLines', s1.eraseLines(['bc','ca','ab']).toJS())
+console.log('eraseLine', s1.lines.erase('ab').toJS())
+console.log('eraseLine', s1.lines.erase('ab').lines.erase('bc').toJS())
+console.log('eraseLines', s1.lines.erase(['bc','ca']).toJS())
+console.log('eraseLines', s1.lines.erase(['bc','ca','ab']).toJS())
